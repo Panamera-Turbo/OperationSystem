017324d1a138ddc9d84cd01cfcd3a76d33215c14
Panamera-Turbo <ethanwithcarreraturbo11567@gmail.com>
2021-06-04 11:23:27 +0800

    os

--- experiment/5-monitor/monitor.sh
+++ experiment/5-monitor/monitor.sh
@@ -1,26 +1,30 @@
-if [ $# -ge 2]
+#!/bin/bash
+if [ $# -ge 2 ]
 then 
     echo "Usage: usr_monitor  username"
+    exit
 fi
 
-if [ $# -eq 0]
+if [ $# -eq 0 ]
 then
     echo "input one username!"
+    exit
 fi
 
 target_name=$1
-
+echo "target user: "$target_name
+echo ""
 name_list=$(who | cut -d" " -f 1)
-echo $name_list  #这里有错，要求是打印name_list
-
+echo $name_list
+echo ""
 while [ 1 ]
 do
-    for $target_name in ${name_list[*]}
+    for target_name in ${name_list[*]}
     do  
-        echo $target_name"logged in"
+        echo $target_name" logged in"
         exit
     done
 
-    echo "waiting user"$target_name"..."
+    echo "waiting user "$target_name"..."
     sleep 1s
 done
